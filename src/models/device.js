b41936fb68c2c8ad77ed939fb9091dd54afcd979
dineshsrisai <dineshsrisai1@gmail.com>
2026-04-17 22:08:46 +0530

    Updated the schema for featured services

--- src/models/device.js
+++ src/models/device.js
@@ -4,6 +4,7 @@ const deviceSchema = new mongoose.Schema({
   title: String,
   description: String,
   image: String,
+  featured : Boolean,
   type: {
     type: String,
     enum: ["printer", "computer", "cctv"],
